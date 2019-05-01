744f83f0a7681f084d38be04b6a29f60c3f935cc
dsoccer1980 <dsoccer1980@gmail.com>
2019-05-01 10:18:29 +0300

    Create methods for getting methods with annotations and invoking methods

--- hw03/src/main/java/ru/dsoccer1980/MyJUnit.java
+++ hw03/src/main/java/ru/dsoccer1980/MyJUnit.java
@@ -8,56 +8,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyJUnit {
+    private List<Method> methodsBeforeEach = new ArrayList<>();
+    private List<Method> methodsAfterEach = new ArrayList<>();
+    private List<Method> methodsBeforeAll = new ArrayList<>();
+    private List<Method> methodsAfterAll = new ArrayList<>();
+    private List<Method> methodsTest = new ArrayList<>();
 
     public static void run(Class<?> testClass) {
-        List<Method> methodsBeforeEach = new ArrayList<>();
-        List<Method> methodsAfterEach = new ArrayList<>();
-        List<Method> methodsBeforeAll = new ArrayList<>();
-        List<Method> methodsAfterAll = new ArrayList<>();
-        List<Method> methodsTest = new ArrayList<>();
+        MyJUnit myJUnit = new MyJUnit();
+
+        getAnnotationMethods(testClass, myJUnit);
+        invokeMethods(testClass, myJUnit);
+    }
+
+    private static void invokeMethods(Class<?> testClass, MyJUnit myJUnit) {
+        if (!myJUnit.methodsTest.isEmpty()) {
+            if (!invokeMethodsFromListByObject(myJUnit.methodsBeforeAll, null)) {
+                invokeMethodsFromListByObject(myJUnit.methodsAfterAll, null);
+            } else {
+                for (Method method : myJUnit.methodsTest) {
+                    Object object = getInstanceOfClass(testClass);
+                    if (!invokeMethodsFromListByObject(myJUnit.methodsBeforeEach, object)) {
+                        invokeMethodsFromListByObject(myJUnit.methodsAfterEach, object);
+                    } else {
+                        invokeMethodByObject(method, object);
+                        invokeMethodsFromListByObject(myJUnit.methodsAfterEach, object);
+                    }
+                }
+                invokeMethodsFromListByObject(myJUnit.methodsAfterAll, null);
+            }
+        }
+    }
+
+    private static void getAnnotationMethods(Class<?> testClass, MyJUnit myJUnit) {
         Method[] declaredMethods = testClass.getDeclaredMethods();
 
         for (Method method : declaredMethods) {
-
             if (method.getAnnotation(AfterEach.class) != null) {
-                methodsAfterEach.add(method);
-            }
-
-            if (method.getAnnotation(BeforeEach.class) != null) {
-                methodsBeforeEach.add(method);
-            }
-
-            if (method.getAnnotation(BeforeAll.class) != null) {
-                methodsBeforeAll.add(method);
-            }
-
-            if (method.getAnnotation(AfterAll.class) != null) {
-                methodsAfterAll.add(method);
-            }
-
-            if (method.getAnnotation(Test.class) != null) {
-                methodsTest.add(method);
-            }
-        }
-
-        if (!methodsTest.isEmpty()) {
-
-            if (!invokeMethodsFromListByObject(methodsBeforeAll, null)) {
-                invokeMethodsFromListByObject(methodsAfterAll, null);
-            } else {
-
-                for (Method method : methodsTest) {
-                    Object object = getInstanceOfClass(testClass);
-                    if (!invokeMethodsFromListByObject(methodsBeforeEach, object)) {
-                        invokeMethodsFromListByObject(methodsAfterEach, object);
-                    } else {
-                        invokeMethodByObject(method, object);
-                        invokeMethodsFromListByObject(methodsAfterEach, object);
-                    }
-
-                }
-
-                invokeMethodsFromListByObject(methodsAfterAll, null);
+                myJUnit.methodsAfterEach.add(method);
+            } else if (method.getAnnotation(BeforeEach.class) != null) {
+                myJUnit.methodsBeforeEach.add(method);
+            } else if (method.getAnnotation(BeforeAll.class) != null) {
+                myJUnit.methodsBeforeAll.add(method);
+            } else if (method.getAnnotation(AfterAll.class) != null) {
+                myJUnit.methodsAfterAll.add(method);
+            } else if (method.getAnnotation(Test.class) != null) {
+                myJUnit.methodsTest.add(method);
             }
         }
     }
