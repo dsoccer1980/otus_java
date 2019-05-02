749fc41bfd114c19e8e001654ce79e5b89f68a69
dsoccer1980 <dsoccer1980@gmail.com>
2019-05-02 18:14:05 +0300

    Take into method searching methods with @Log

--- hw04/src/main/java/ru/dsoccer1980/IoC.java
+++ hw04/src/main/java/ru/dsoccer1980/IoC.java
@@ -3,6 +3,8 @@ package ru.dsoccer1980;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
 
 class IoC {
 
@@ -15,20 +17,29 @@ class IoC {
 
     static class DemoInvocationHandler implements InvocationHandler {
         private final ITestLogging testLogging;
+        private List<String> logMethods = new ArrayList<>();
 
         DemoInvocationHandler(ITestLogging testLogging) {
             this.testLogging = testLogging;
+            findLogMethods();
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (logMethods.contains(method.getName())) {
+                System.out.println(String.format("executed method: %s, param: %s ", method.getName(), args[0]));
+            }
+
+            return method.invoke(testLogging, args);
+        }
+
+        private void findLogMethods() {
             Method[] declaredMethods = testLogging.getClass().getDeclaredMethods();
             for (Method declaredMethod : declaredMethods) {
-                if (declaredMethod.getName().equals(method.getName()) && declaredMethod.getAnnotation(Log.class) != null) {
-                    System.out.println(String.format("executed method: %s, param: %s ", method.getName(), args[0]));
+                if (declaredMethod.getAnnotation(Log.class) != null) {
+                    logMethods.add(declaredMethod.getName());
                 }
             }
-            return method.invoke(testLogging, args);
         }
     }
 }
