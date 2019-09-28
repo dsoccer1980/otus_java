897a6b3ae1f99b28e97b0eb75d39b76dfb2eac75
dsoccer1980 <dsoccer1980@gmail.com>
2019-09-28 22:35:49 +0300

    Refactor

--- hw16/hw16_ms/src/main/java/ru/dsoccer1980/messageSystem/MessageSystemImpl.java
+++ hw16/hw16_ms/src/main/java/ru/dsoccer1980/messageSystem/MessageSystemImpl.java
@@ -1,6 +1,7 @@
 package ru.dsoccer1980.messageSystem;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -15,64 +16,63 @@ import java.util.concurrent.LinkedBlockingQueue;
 @Service
 public class MessageSystemImpl implements MessageSystem {
 
-    private static final int PORT_F = 5050;
-    private static final int PORT_DB = 5051;
     private final int NUMBER_THREAD_IN_POOL = 3;
     private final LinkedBlockingQueue<Client> frontendClients = new LinkedBlockingQueue<>();
-    private final LinkedBlockingQueue<ClientDB> databaseClients = new LinkedBlockingQueue<>();
+    private final LinkedBlockingQueue<Client> databaseClients = new LinkedBlockingQueue<>();
+    @Value("${port.frontend}")
+    private int PORT_F;
+    @Value("${port.db}")
+    private int PORT_DB;
     private ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_THREAD_IN_POOL);
-    private Socket clientSocketDB;
 
     public void start() {
         server();
     }
 
     private void server() {
-        Thread thread = new Thread(() -> {
-            try (ServerSocket serverSocket = new ServerSocket(PORT_F)) {
-                while (!serverSocket.isClosed()) {
-                    Socket clientSocket = serverSocket.accept();
-                    System.out.println("connected: " + clientSocket);
-                    Client client = new Client(clientSocket);
-                    frontendClients.put(client);
-                    executorService.execute(client);
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            System.out.println("server closed");
-        });
-        thread.start();
-
-        Thread threadDb = new Thread(() -> {
-            try (ServerSocket serverSocket = new ServerSocket(PORT_DB)) {
-                while (!serverSocket.isClosed()) {
-                    Socket clientSocket = serverSocket.accept();
-                    clientSocketDB = clientSocket;
-                    System.out.println("connected: " + clientSocket);
-                    ClientDB client = new ClientDB(clientSocket);
-                    databaseClients.put(client);
-                    executorService.execute(client);
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            System.out.println("server closed");
-        });
-        threadDb.start();
-
+        new ServerThread(PORT_F, databaseClients, frontendClients).start();
+        new ServerThread(PORT_DB, frontendClients, databaseClients).start();
     }
 
+    private class ServerThread extends Thread {
+        private int port;
+        private LinkedBlockingQueue<Client> queueForSend;
+        private LinkedBlockingQueue<Client> queueForReceive;
+
+        public ServerThread(int port, LinkedBlockingQueue<Client> queueForSend, LinkedBlockingQueue<Client> queueForReceive) {
+            this.port = port;
+            this.queueForSend = queueForSend;
+            this.queueForReceive = queueForReceive;
+        }
+
+        @Override
+        public void run() {
+            try (ServerSocket serverSocket = new ServerSocket(port)) {
+                while (!serverSocket.isClosed()) {
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("connected: " + clientSocket);
+                    Client client = new Client(clientSocket, queueForSend);
+                    queueForReceive.put(client);
+                    executorService.execute(client);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            System.out.println("server closed");
+        }
+    }
 
     private class Client implements Runnable {
         private Socket clientSocket;
+        private LinkedBlockingQueue<Client> queueForSend;
         @Getter
         private PrintWriter out;
         @Getter
         private BufferedReader in;
 
-        Client(Socket clientSocket) {
+        Client(Socket clientSocket, LinkedBlockingQueue<Client> queueForSend) {
             this.clientSocket = clientSocket;
+            this.queueForSend = queueForSend;
         }
 
         @Override
@@ -87,7 +87,7 @@ public class MessageSystemImpl implements MessageSystem {
                     while (input == null) {
                         input = in.readLine();
                         if (input != null) {
-                            ClientDB client = databaseClients.peek();
+                            Client client = queueForSend.peek();
                             if (client != null) {
                                 client.out.println(input);
                             }
@@ -97,47 +97,7 @@ public class MessageSystemImpl implements MessageSystem {
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-
             System.out.println("clientSocket closed");
         }
     }
-
-    private class ClientDB implements Runnable {
-        private Socket clientSocket;
-        @Getter
-        private PrintWriter out;
-        @Getter
-        private BufferedReader in;
-
-        ClientDB(Socket clientSocket) {
-            this.clientSocket = clientSocket;
-        }
-
-        @Override
-        public void run() {
-            try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-            ) {
-                this.out = out;
-                this.in = in;
-                while (true) {
-                    String input = null;
-                    while (input == null) {
-                        input = in.readLine();
-                        if (input != null) {
-                            Client client = frontendClients.peek();
-                            if (client != null) {
-                                client.out.println(input);
-                            }
-                        }
-                    }
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-
-            System.out.println("clientSocket closed");
-        }
-    }
-
 }
